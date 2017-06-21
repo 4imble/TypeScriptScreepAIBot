391e014cefbe7708342f7634d3209e07d85b95fc
4imble <gimble.net@gmail.com>
2017-06-21 23:19:18 +0100

    Now has working typescript via gulp and flat structure generation without intermediate js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
 var gulp = require('gulp');
 var flatten = require('gulp-flatten');
 var flattenRequires = require('gulp-flatten-requires');
+var ts = require('gulp-typescript');
 
-gulp.task('default', function() {
-  gulp.src(['src/**/*.js'])
+var tsProject = ts.createProject('./tsconfig.json');
+
+gulp.task('default', ['build']);
+
+gulp.task('build', function() {
+  gulp.src(['src/**/*.ts', 'typings/globals/**/*.d.ts'])
+  .pipe(tsProject())
   .pipe(flatten())
   .pipe(flattenRequires())
   .pipe(gulp.dest('./'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/**/*.ts', ['build'])
 });
